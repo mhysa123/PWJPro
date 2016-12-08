9d6d53dc76d9f7a8eadcae5ae81e407cce6e0f53
mhysa <mhysa123@hotmail.com>
2016-12-08 17:31:08 +0800

    测试

--- app/src/main/java/com/example/administrator/pwjproject/MainActivity.java
+++ app/src/main/java/com/example/administrator/pwjproject/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.administrator.pwjproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.tv_hello)
+    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
